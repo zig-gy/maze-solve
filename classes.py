52357ba4165f3d54e7a8c1b7cf13aaf602970115
zig-gy <benjamin.f.chadwick@proton.me>
2025-05-07 13:27:20 -0400

    changed window.py to classes.py and added the Point, Line and Cell classes

---
+++ classes.py
@@ -0,0 +1,83 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("Hola mundo")
+        self.canvas = Canvas(self.root, width=self.width, height=self.height, bg="white")
+        self.canvas.pack()
+        self.is_running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.root.update()
+        self.root.update_idletasks()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+        
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+        
+    def draw(self, canvas, fill_color):
+        x1 = self.point1.x
+        x2 = self.point2.x
+        y1 = self.point1.y
+        y2 = self.point2.y
+        canvas.create_line(x1,y1,x2,y2, fill=fill_color, width=2)
+
+class Cell():
+    def __init__(self, win, point1, point2):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._win = win
+        if point1.x > point2.x:
+            self._x1 = point2.x
+            self._x2 = point1.x
+        else:
+            self._x1 = point1.x
+            self._x2 = point2.x
+        if point1.y > point2.y:
+            self._y1 = point2.y
+            self._y2 = point1.y
+        else:
+            self._y1 = point1.y
+            self._y2 = point2.y
+        
+    def draw(self):        
+        top_left = Point(self._x1, self._y1)
+        top_right = Point(self._x2, self._y1)
+        bot_left = Point(self._x1, self._y2)
+        bot_right = Point(self._x2, self._y2)
+        
+        if self.has_bottom_wall:
+            wall = Line(bot_left, bot_right)
+            self._win.draw_line(wall, "black")
+        if self.has_left_wall:
+            wall = Line(top_left, bot_left)
+            self._win.draw_line(wall, "blue")
+        if self.has_right_wall:
+            wall = Line(top_right, bot_right)
+            self._win.draw_line(wall, "red")
+        if self.has_top_wall:
+            wall = Line(top_left, top_right)
+            self._win.draw_line(wall, "yellow")
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from window import Window
+from classes import Window, Line, Point, Cell
 
-def __main__():
-    win = Window(800, 600)
-    win.wait_for_close()
+def main():
+    window = Window(800, 600)
+    
+    line1 = Line(Point(22,22), Point(44,44))
+    line2 = Line(Point(45,30), Point(500,500))
+    
+    window.draw_line(line1, "black")
+    window.draw_line(line2, "red")
+    
+    cell = Cell(window, Point(300,300), Point(500, 500))
+    cell.has_top_wall = False
+    cell.draw()
+    
+    window.wait_for_close()
 
-if __name__ == __main__:
-    __main__()
-
+if __name__ == "__main__":
+    main()
