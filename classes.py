63b21f365ee5ab28ebba7dd995c5ee437766e00e
zig-gy <benjamin.f.chadwick@proton.me>
2025-05-07 15:18:05 -0400

    added maze class

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import time
+
 from tkinter import Tk, BOTH, Canvas
 
 class Window():
@@ -91,3 +93,52 @@ class Cell():
         color = "gray" if undo else "red"
         
         self._win.draw_line(Line(center_self, center_to), color)
+
+class Maze():
+    _cells = []
+    
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._create_cells()
+    
+    def _create_cells(self):
+        current_x = self.x1
+        current_y = self.y1
+        for col in range(self.num_cols):
+            row_cells = []
+            for row in range(self.num_rows):
+                top_left = Point(current_x, current_y)
+                current_x += self.cell_size_x
+                bot_right = Point(current_x, current_y + self.cell_size_y)
+                row_cells.append(Cell(self.win, top_left, bot_right))
+            self._cells.append(row_cells)
+            current_y += self.cell_size_y
+            current_x = self.x1
+        
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._draw_cell(i,j)
+    
+    def _draw_cell(self, i, j):
+        cell = self._cells[i][j]
+        cell.draw()
+        self._animate()
+    
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.1)
--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-from classes import Window, Line, Point, Cell
+from classes import Window, Maze
 
 def main():
     window = Window(800, 600)
     
-    cell = Cell(window, Point(300,300), Point(500, 500))
-    cell.has_top_wall = False
-    cell.draw()
-    
-    cell2 = Cell(window, Point(300,100), Point(500, 300))
-    cell2.has_bottom_wall = False
-    cell2.draw()
-    
-    cell.draw_move(cell2)
+    Maze(150, 40, 10, 10, 50, 50, window)
     
     window.wait_for_close()
 
